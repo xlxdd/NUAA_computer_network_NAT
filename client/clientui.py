0b10ab5a44d7b88e6a00d59d6ad431edb9535db5
xlxdd <393025885@qq.com>
2023-06-01 21:30:22 +0800

    优化文件结构，编写readme文件。

--- client/clientui.py
+++ client/clientui.py
@@ -105,22 +105,3 @@ class Ui_MainWindow(object):
         self.clientIDlabel.setText(_translate("MainWindow", "本进程标识"))
         self.switcherIPlabel.setText(_translate("MainWindow", "交换机：IP地址"))
         self.sendbutton.setText(_translate("MainWindow", "发送"))
-    '''def sendandrcvmessage(self, MainWindow):
-        self.showtext.setText("关闭")
-        pass
-    '''
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv) # 创建一个QApplication，也就是你要开发的软件app
-    MainWindow = QtWidgets.QMainWindow()  # 创建一个QMainWindow，用来装载你需要的各种组件、控件
-    ui = Ui_MainWindow()          # ui是Ui_MainWindow()类的实例化对象
-    ui.setupUi(MainWindow)         # 执行类中的setupUi方法，方法的参数是第二步中创建的QMainWindow
-    MainWindow.show()            # 执行QMainWindow的show()方法，显示这个QMainWindow
-    sys.exit(app.exec_())          # 使用exit()或者点击关闭按钮退出QApplicat
-'''if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    stats = Ui_MainWindow()
-    stats.window.show()
-    app.exec_()
-'''
-
